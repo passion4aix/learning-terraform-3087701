8c296fcde4031d950e3d3ddc03647a759339d682
passion4aix <passion4aix@gmail.com>
2023-02-25 10:18:41 +0530

    fix typo

--- variables.tf
+++ variables.tf
@@ -10,9 +10,10 @@ variable "ami_filter" {
     name  = string
     owner = string 
   })
+
   default = {
     name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
-    owner = ""979382823631" # Bitnami 
+    owner = "979382823631" # Bitnami 
   }
 }
 
